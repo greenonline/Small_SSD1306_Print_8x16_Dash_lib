e6b343a6eb856d3785261bb7466155e55988379a
greenonline <greenonline@users.noreply.github.com>
2017-09-27 23:17:06 +0700

    Create Small_SSD1306_Print_8x16_Dash_lib_HelloWorld_F.ino

---
+++ examples/Small_SSD1306_Print_8x16_Dash_lib_HelloWorld_F/Small_SSD1306_Print_8x16_Dash_lib_HelloWorld_F.ino
@@ -0,0 +1,46 @@
+// Small_SSD1306_Print_8x16_Dashed_lib_HelloWorld_F.ino
+// Prints "Hello World" using string in Flash F()
+//
+// connect display SCL to pin A5, and SDA to pin A4
+// connect LED to pin 4 (it will blink in case of I2C error)
+
+#include <Small_SSD1306_Print_8x16_Dash_lib.h>
+
+Small_SSD1306_Print_8x16_Dash_lib lcd = Small_SSD1306_Print_8x16_Dash_lib();
+
+void setup() {
+  lcd.begin();
+  lcd.setCursorMode(true);
+}
+
+void loop() {
+  printHelloWorld();
+  lcd.D_DRAW_VERT_DASHED(120,0,31);
+  delay(2000);
+  scrollHelloWorld();
+  lcd.setPrint8x16CharacterMode(false);
+  printHelloWorld();
+  delay(2000);
+  scrollHelloWorld();
+  lcd.setPrint8x16CharacterMode(true);
+}
+
+void printHelloWorld(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(F("Hello"));
+  lcd.setCursor(9,1);
+  lcd.print(F("World"));
+}
+
+void scrollHelloWorld(){
+  lcd.clear();
+  for (int x=0;x<11;x++)
+    for (int y=0;y<3;y++){
+      lcd.setCursor(x,y);
+      lcd.print(" Hello ");
+      lcd.setCursor(x+1,y+1);
+      lcd.print(" World ");
+      delay(100);
+    }
+}
